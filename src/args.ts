8201cc124bfdf00735125c4eae6ca57557f78133
Jaxel Rojas <jaxelr@gmail.com>
2022-04-15 10:40:20 -0400

    feat: added comments to args example

--- src/args.ts
+++ src/args.ts
@@ -1,9 +1,11 @@
+import { parse } from "https://deno.land/std@0.119.0/flags/mod.ts";
+
+// Get the list of arguments passed to the program
 const name = Deno.args[0];
 const food = Deno.args[1];
 console.log(`Hello ${name}, I like ${food}!`);
 
-import { parse } from "https://deno.land/std@0.119.0/flags/mod.ts";
-
+// You need to parse the arguments from the file 
 const flags = parse(Deno.args, {
   boolean: ["help", "color"],
   string: ["version"],
@@ -13,4 +15,5 @@ console.log("Wants help?", flags.help);
 console.log("Version:", flags.version);
 console.log("Wants color?:", flags.color);
 
+// All arguments not recognized as flags
 console.log("Other:", flags._);

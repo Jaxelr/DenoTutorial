ceb9f0c87eabd2b6f7b5bd50a86ce4b4d1d3134b
Jaxel Rojas <jaxelr@gmail.com>
2022-04-21 10:01:24 -0400

    feat: added comments to server2.ts file

--- src/server2.ts
+++ src/server2.ts
@@ -1,7 +1,10 @@
 import { listenAndServe } from "https://deno.land/std@0.109.0/http/server.ts";
  
 const addr = ":8080";
- 
+
+// Http server needs a handler function
+// this function is called for every request that comes in
+// it works asynchronously
 const handler = (request: Request): Response => {
   let body = "Your user--agent is:\n\n";
   body += request.headers.get("user-agent") || "Unknown";

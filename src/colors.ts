6051239eb110e066a498b403ba34ac813608b760
Jaxel Rojas <jaxelr@gmail.com>
2022-04-24 10:21:06 -0400

    style: added comments to examples

--- src/colors.ts
+++ src/colors.ts
@@ -1,17 +1,24 @@
 //Taken from https://examples.deno.land/color-logging
 
+// CSS Support
 console.log("%cHello World", "color: red");
 
+// Background Color
 console.log("%cHello World", "background-color: blue");
 
+// Text Decorations
 console.log("%cHello World", "text-decoration: underline");
 console.log("%cHello World", "text-decoration: line-through");
 
+// Font Weight
 console.log("%cHello World", "font-weight: bold");
 
+// Multiple CSS Properties
 console.log("%cHello World", "color: red; font-weight: bold");
 
+// Manage multiple values
 console.log("%cHello %cWorld", "color: red", "color: blue");
 
+// RGB Color are also supported
 console.log("%cHello World", "color: #FFC0CB");
 console.log("%cHello World", "color: rgb(255, 192, 203)");

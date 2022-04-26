47c2e02e29e3b8b9915d38c9a73cb5169cd00526
Jaxel Rojas <jaxelr@gmail.com>
2022-04-26 08:26:36 -0400

    Added comments to import and export sample modules.

--- src/import.ts
+++ src/import.ts
@@ -1,8 +1,11 @@
+// Import things from other files using the import keyword
 import { sayHello } from "./util.ts";
 sayHello("World");
 
+// Import all things from a file using the *
 import * as util from "./util.ts";
 util.sayHello("World");
 
+// You can also reference absolute paths or http(s):// URLs
 import { VERSION } from "https://deno.land/std/version.ts";
 console.log(VERSION);
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,8 @@
 export function sayHello(thing: string) {
     console.log(`Hello, ${thing}!`);
   }
-  
+
+  // Use export keyword to make a function available outside of the module.
   export interface Foo {}
   export class Bar {}
   export const baz = "baz";

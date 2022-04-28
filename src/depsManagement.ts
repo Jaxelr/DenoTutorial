43ef45035a154022af44ddb702341478fb929d9f
Jaxel Rojas <jaxelr@gmail.com>
2022-04-27 22:41:50 -0400

    Added comments to management sample

--- src/depsManagement.ts
+++ src/depsManagement.ts
@@ -1,2 +1,3 @@
+// Re-export all remote dependencies
 export * as http from "https://deno.land/std@0.119.0/http/mod.ts";
 export * as path from "https://deno.land/std@0.119.0/path/mod.ts";
--- src/management.ts
+++ src/management.ts
@@ -1,5 +1,7 @@
+// Import dependencies from the depsManagement file
 import { path, http } from "./depsManagement.ts";
 
+// Keep them separated and cohesive
 console.log(path);
 console.log(http);
 

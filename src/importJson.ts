a3e7f6c4a1d035dd699bba5801b8e5f22764a31c
Jaxel Rojas <jaxelr@gmail.com>
2022-04-29 13:54:46 -0400

    Added comments to import json sample

--- src/importJson.ts
+++ src/importJson.ts
@@ -1,6 +1,9 @@
+// json files can be imported but you need to specify the type json
 import file from "./version.json" assert { type: "json" };
+
 console.log(`version is ${file.version}`);
 
+// dynamic import are also supported
 const module = await import("./version.json", {
   assert: { type: "json" },
 });

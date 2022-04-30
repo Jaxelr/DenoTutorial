d4658aa2a50b88b0214f21681287a8d08e9f8a3a
Jaxel Rojas <jaxelr@gmail.com>
2022-04-30 08:32:27 -0400

    Added comments to json parser

--- src/parseJson.ts
+++ src/parseJson.ts
@@ -3,6 +3,7 @@ const text = `{
     "numbers": [1, 2, 3]
   }`;
   
+// built-in json parser
 const data = JSON.parse(text);
 console.log(data.hello);
 console.log(data.numbers.length);
@@ -11,8 +12,11 @@ const obj = {
     hello: "world",
     numbers: [1, 2, 3],
 };
+
+// built-in json stringifier
 const json = JSON.stringify(obj);
 console.log(json);
 
+// built-in json stringifier with indentation
 const json2 = JSON.stringify(obj, null, 2);
 console.log(json2);

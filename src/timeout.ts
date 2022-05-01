0fad2010c9fa5722c0ecb0df6f7cd02a479dc52b
Jaxel Rojas <jaxelr@gmail.com>
2022-05-01 11:54:09 -0400

    Added comments ot the timeout example

--- src/timeout.ts
+++ src/timeout.ts
@@ -1,10 +1,13 @@
+// Create timer that will print hello world after 1 second
 setTimeout(() => console.log("Hello, World!"), 1000);
 
+// Cancel a timer after it has been created
 const timerId = setTimeout(() => console.log("No!"), 1000);
 clearTimeout(timerId);
 
-/* Careful this is an infinite loop! */
+// Careful this is an infinite loop! Don't do this!
 setInterval(() => console.log("Hey!"), 1000);
 
+// Intervals can be cancelled
 const intervalId = setInterval(() => console.log("Nope"), 1000);
 clearInterval(intervalId);

146f13c95242bf62245f9c31a861ce17e4f12506
Jaxel Rojas <jaxelr@gmail.com>
2022-05-03 07:16:56 -0400

    Added comments to the input Prompt and version examples

--- src/inputPrompts.ts
+++ src/inputPrompts.ts
@@ -1,11 +1,15 @@
+// Alert & acknowledge
 alert("Please acknowledge the message.");
 console.log("The message has been acknowledged.");
 
+// Ask for user confirmation
 const shouldProceed = confirm("Do you want to proceed?");
 console.log("Should proceed?", shouldProceed);
 
+// Prompt user for input
 const name = prompt("Please enter your name:");
 console.log("Name:", name);
 
+// Specify a default value from prompt
 const age = prompt("Please enter your age:", "18");
 console.log("Age:", age);
--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,6 @@
+// Deno global object
 console.log("Current Deno version", Deno.version.deno);
 
+// Deno dependencies, v8 and typescript
 console.log("Current TypeScript version", Deno.version.typescript);
 console.log("Current V8 version", Deno.version.v8);

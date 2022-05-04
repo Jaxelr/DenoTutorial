17da9285d272f71d78975ee062cc17b7c873838a
Jaxel Rojas <jaxelr@gmail.com>
2022-05-04 07:21:28 -0400

    Added comments to environment variables examples.

--- src/envVar.ts
+++ src/envVar.ts
@@ -1,13 +1,21 @@
+// These can be access only with the --allow-env flag
+
+// Read environment variable
 const PORT = Deno.env.get("PORT");
 console.log("PORT:", PORT);
 
+// Get object reading all environment variables
 const env = Deno.env.toObject();
 console.log("env:", env);
 
+// Set environment variable, only affects the current process
 Deno.env.set("MY_PASSWORD", "123456");
 
+// Unset environment variable
 Deno.env.delete("MY_PASSWORD");
 
+//Enrivonment variables are case-sensitive on unix-like systems, 
+// but not on Windows.
 Deno.env.set("MY_PASSWORD", "123");
 Deno.env.set("my_password", "456");
 console.log("UPPERCASE:", Deno.env.get("MY_PASSWORD"));
